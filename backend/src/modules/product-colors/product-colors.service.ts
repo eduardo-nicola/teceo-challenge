13276a0df4f334096af967c1f76b206568986a07
eduardo-nicola <eduardonicolagit@gmail.com>
2025-12-29 11:25:31 -0300

    feat: remove unnecessary empty check for product colors in list method

--- backend/src/modules/product-colors/product-colors.service.ts
+++ backend/src/modules/product-colors/product-colors.service.ts
@@ -32,10 +32,6 @@ export default class ProductColorsService {
 
     const productColors = await queryBuilder.getMany();
 
-    if (productColors.length === 0) {
-      return Page.of([], countQueryBuilder);
-    }
-
     const ids = productColors.map(pc => pc.id);
     const prices = await this.createQueryBuilder()
       .select('productColor.id', 'id')
